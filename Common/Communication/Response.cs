b6324e327c17f57c201e8de040a2e01bc5212c88
SinhroLK <lkrstic001@gmail.com>
2024-01-18 14:49:37 +0100

    Added Operation, Response and Request classes in Communication

---
+++ Common/Communication/Response.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Communication
+{
+    [Serializable]
+    public class Response
+    {
+        public object Odgovor { get; set; }
+        public Operation Operacija { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
